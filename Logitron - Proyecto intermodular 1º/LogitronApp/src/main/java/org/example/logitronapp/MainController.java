296394f71d3f74970bc9b6d9159788df82733c0b
Patricia Forssen 2 <elinpatricia27@gmail.com>
2026-03-10 23:54:19 +0100

    Primer dia trabajnado en ello
    yea

--- Logitron - Proyecto intermodular 1º/LogitronApp/src/main/java/org/example/logitronapp/MainController.java
+++ Logitron - Proyecto intermodular 1º/LogitronApp/src/main/java/org/example/logitronapp/MainController.java
@@ -4,23 +4,47 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
-    private Button btnSaludo;
+    private Button btnAcceso, btnNuevaClave;
 
     @FXML
-    private TextField editNombre;
+    private TextField editUsuario, editClave;
 
     @FXML
     private VBox vBox;
 
+    @FXML
+    private HBox hBox;
+
+    @FXML
+    private Text txtFeedback;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("Ejemplo app java fx");
+    public void initialize(URL location, ResourceBundle resources) { actions();}
+
+    private void actions() {
+
+        btnAcceso.setOnAction(actionEvent -> {
+
+            String user = editUsuario.getText().toLowerCase().trim();
+            String clave = editClave.getText();
+
+            if (user.isEmpty()  || clave.isEmpty()) {
+                txtFeedback.setText("Escribe un nombre de usuario y una clave");
+            } else if (user.equals("patricia") && clave.equals("clave")) {
+                txtFeedback.setText(String.format("Bienvenida %s", user));
+            } else {
+                txtFeedback.setText("El usuario no existe o la clave es incorrecta");
+            }
+
+        });
     }
 }
